3ca65d203cdb2076d62e84fc41313863b042cb6f
ayush <ayushdeveloper0711@gmail.com>
2021-07-09 14:46:06 +0530

    Updated

--- ParseData/app/src/main/java/com/ayush/parsedata/MainActivity.java
+++ ParseData/app/src/main/java/com/ayush/parsedata/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.VoiceInteractor;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,24 +17,66 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+import java.time.LocalDate;
 
+public class MainActivity extends AppCompatActivity {
+TextView textView;
 String url="https://www.google.com";
 String apiUrl="https://jsonplaceholder.typicode.com/todos";
-    RequestQueue queue;
+String objectApiUrl="https://jsonplaceholder.typicode.com/todos/1";
+    //RequestQueue queue;
+    RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        queue= Volley.newRequestQueue(this);
-        JsonArrayRequest jsonObjectRequest=new JsonArrayRequest(Request.Method.GET, apiUrl, null, response -> Log.d("JSON",response.toString().substring(0,500)), error -> Log.d("JSON","Failed"));
-        queue.add(jsonObjectRequest);
-        getString(queue);
+        textView=findViewById(R.id.textView);
+        requestQueue=MySingleton.getInstance(this).getRequestQueue();
+        //queue= Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET, objectApiUrl, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    textView.setText(response.getString("title"));
+                    Log.d("jsonObj",response.getString("title"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("jsonObj","Failed");
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+        //queue.add(jsonObjectRequest);
+        //getJsonArrayRequest();
+        //getString(queue);
     }
 
-    private void getString(RequestQueue queue) {
+    private void getJsonArrayRequest() {
+        JsonArrayRequest jsonObjectRequest=new JsonArrayRequest(Request.Method.GET, apiUrl, null, response ->
+        {
+            for (int i = 0; i < response.length(); i++) {
+                try {
+                    JSONObject jsonObject=response.getJSONObject(i);
+
+                    Log.d("JSON",jsonObject.getString("title"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        },error -> Log.d("JSON","Failed"));
+       // queue.add(jsonObjectRequest);
+    }
+
+    /*private void getString(RequestQueue queue) {
         StringRequest stringRequest=new StringRequest(Request.Method.GET, url, response -> Log.d("MainData",response.substring(0,500)), new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
@@ -41,5 +84,5 @@ String apiUrl="https://jsonplaceholder.typicode.com/todos";
             }
         });
         queue.add(stringRequest);
-    }
+    }*/
 }
